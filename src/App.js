e57b94b0c66f299cdcc831973539c39455dba1b8
Obie Carnathan <aikia16@uab.edu>
2024-01-02 21:16:08 -0600

    Removed REACT template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import photo from './GradPic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-
-          A website for my personal projects and resume is coming soon!        </p>
-        <a
-          className="App-link"
-          // href="https://reactjs.org"
-          href="https://github.com/ocarnathan"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </header>
+      <h1>Personal Website</h1>
+      <p>Under construction</p>
     </div>
   );
 }
