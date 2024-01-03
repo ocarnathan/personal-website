a0f2a61f393ab2fd7ca250390fe63e67a344f351
Obie Carnathan <aikia16@uab.edu>
2024-01-03 14:10:41 -0600

    Navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'; // import React from 'react';
+import ReactDOM from 'react-dom/client'; // import ReactDOM from 'react-dom';
+import App from './App'; // import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* This is the entry point for the React app.
+ The root variable is used to render the App component to the DOM.
+ The DOM is the Document Object Model, which is a programming interface for HTML and XML documents.
+ */
+const root = ReactDOM.createRoot(document.getElementById('root')); // const root = document.getElementById('root');
 root.render(
     <App />
 );
