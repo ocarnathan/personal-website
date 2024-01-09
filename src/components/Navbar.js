5b18fe25014c9a1a7d8482f094d10a57e4b6e8ef
Obie Carnathan <aikia16@uab.edu>
2024-01-08 19:35:06 -0600

    github and resume added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,9 @@ function Navbar() {
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="#">About Me</a></li>
                         <li><a href="#">Projects</a></li>
-                        <li><a href="#">Resume</a></li>
+                        <li><a href="https://drive.google.com/file/d/1Ujq-Cm0T9eHzVPJrGGnzKeM6G9e6Xge7/view?usp=drive_link" target="_blank">Resume</a></li>
                         <li><a href="#">Contact Me</a></li>
+                        <li><a href="https://github.com/ocarnathan" target="_blank">GitHub</a></li>
                     </ul>
                 </div>
             </nav>
