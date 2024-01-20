08dad48cb4b3ea32f7748e6d71d435fb557fe848
Obie Carnathan <aikia16@uab.edu>
2024-01-19 19:33:25 -0600

    partial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import photo from './GradPic.jpg';
+import photo from './Grad_Pic.png';
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Projects from './Projects';
+import AboutMe from './AboutMe';
+import ContactMe from './ContactMe';
+import logo from './images/logo2.png';
+import user from './images/runner2.jpeg';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
           <Route path="/projects" element={<Projects />} />
+          <Route path="/AboutMe" element={<AboutMe />} />
+          <Route path="/ContactMe" element={<ContactMe />} />
           <Route path="/" element={
             <>
-              <h1>Welcome to my portfolio</h1>
-              <p>Under construction</p>
-              <img src={photo} alt="Grad Pic" class="Grad_Pic"></img>
-              <p>
+              {/* <h1 className="Hello">Hi, my name is Obie</h1> */}
+              {/* <p>Under construction</p> */}
+            <div className="header" id="header">
+              <div className="container" id="container"> 
+                <nav>
+                  <img src={logo} class="logo"></img>
+                  <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                  </ul>
+                </nav>
+                <div className="header-text">
+                  <p>Software Engineer</p>
+                  <h1>Hi, I'm <span>Obie</span> <br></br> Carnathan from Birmingham, AL</h1>
+                </div>
+              </div>
+
+              <div id="about">
+                <div className="container">
+                  <div className="row">
+                    <div className="about-column-1">
+                      <img src={user}></img>
+                    </div>
+                    <div className="about-column-2">
+                      <h1>About Me</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* <p className="Bio">
         Welcome to my portfolio! I'm a recent graduate from the University of Alabama at Birmingham, 
         where I earned a Bachelor's degree in computer science. My journey has been diverse—I proudly 
         served in the Marine Corps before diving into the world of technology. Driven by a passion for 
@@ -28,9 +63,12 @@ function App() {
         professional journey, featuring a collection of projects that highlight my technical skills 
         and dedication. I invite you to explore and connect as I embark on the next phase of my career, 
         ready to tackle new challenges and make meaningful contributions in the world of software engineering.
-              </p>
-              <iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/46847031/latest-rides/4f4353f485c18a89826633b4d1ba8ccac731aa8d'></iframe>
-
+              </p> */}
+              {/* <img src={photo} alt="Grad Pic" className="Grad_Pic"></img> */}
+              </div>
+              {/* <div className="strava">
+            <iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/46847031/latest-rides/4f4353f485c18a89826633b4d1ba8ccac731aa8d'></iframe>
+              </div> */}
             </>
           } />
         </Routes>
