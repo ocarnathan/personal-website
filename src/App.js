c6bd98b8f2575e1e2b2ba529aa95f55c79add7b7
Obie Carnathan <aikia16@uab.edu>
2024-01-27 10:35:00 -0600

    projects and services

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './images/logo2.png';
 import user from './images/runner2.jpeg';
+import work1 from './images/work-1.png'
+import work2 from './images/work-2.png'
+import work3 from './images/work-3.png'
 import {useState} from 'react';
 
 function App() {
@@ -29,7 +32,7 @@ function App() {
             <div className="header" id="header">
               <div className="container" id="container"> 
                 <nav>
-                  <img src={logo} class="logo"></img>
+                  <img src={logo} className="logo"></img>
                   <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
@@ -88,23 +91,97 @@ function App() {
                             </div>
                             <div className={activeTab === 'education' ? 'tab-contents active-tab' : 'tab-contents'} id="education">
                               <ul>
-                                <li><span>2020-2023</span><br></br>Bachelor's of Science in C.S.</li>
+                                <li><span>2020-2023</span><br></br>Bachelor's of Science in Computer Science</li>
                                 {/* <li><span>Web Development</span><br></br>Web App Development</li>
                                 <li><span>App Development</span><br></br>Building Android/IOS apps</li> */}
                               </ul>
                             </div>
                             {/* <iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/46847031/latest-rides/4f4353f485c18a89826633b4d1ba8ccac731aa8d'></iframe> */}
                         </div>
-                        
                       </div>
                   </div>
               </div>
+{/* -------------Services---------------- */}         
+              <div id="services">
+              <div className="container">
+                <h1 className="sub-title">My Services</h1>
+                <div className="services-list">
+                  <div>
+                  <i className="fa-solid fa-laptop-code"></i>
+                    <h2>Web Design</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Aliquam tristique ornare dictum. Nunc quis magna neque. 
+                      Suspendisse pretium, nibh non tempor porttitor, nunc ipsum 
+                      scelerisque lacus, sed tristique turpis turpis et massa. 
+                      </p>
+                    <a href="#">learn more</a>
+                  </div>
+                  <div>
+                  <i className="fa-solid fa-crop"></i>
+                    <h2>UI/UX design</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Aliquam tristique ornare dictum. Nunc quis magna neque. 
+                      Suspendisse pretium, nibh non tempor porttitor, nunc ipsum 
+                      scelerisque lacus, sed tristique turpis turpis et massa. 
+                      </p>
+                    <a href="#">learn more</a>
+                  </div>
+                  <div>
+                  <i className="fa-brands fa-app-store"></i>
+                    <h2>Web Design</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Aliquam tristique ornare dictum. Nunc quis magna neque. 
+                      Suspendisse pretium, nibh non tempor porttitor, nunc ipsum 
+                      scelerisque lacus, sed tristique turpis turpis et massa. 
+                      </p>
+                    <a href="#">learn more</a>
+                  </div>
+                </div>
+              </div>
             </div>
-            
+{/* -------------portfolio---------------- */}
+            <div id="portfolio">
+              <div className="container">
+                <h1 className="sub-title">My Work/Projects</h1>
+                <div className="work-list">
+                  <div className="work">
+                    <img src={work1} alt="work-1"></img>
+                    <div className="layer">
+                      <h3>Social Media App</h3>
+                      <p>The app connects you to the talented people around the world.
+                        Download it from the play store.</p>
+                        <a href=""><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                    </div>
+                  </div>
+                  <div className="work">
+                    <img src={work2} alt="work-2"></img>
+                    <div className="layer">
+                      <h3>Music App</h3>
+                      <p>The app connects you to the talented people around the world.
+                        Download it from the play store.</p>
+                        <a href=""><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                    </div>
+                  </div>
+                  <div className="work">
+                    <img src={work3} alt="work-3"></img>
+                    <div className="layer">
+                      <h3>Online Shopping App</h3>
+                      <p>The app connects you to the talented people around the world.
+                        Download it from the play store.</p>
+                        <a href=""><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  
             </>
           } />
+          
         </Routes>
       </div>
+      
     </Router>
   );
 }
